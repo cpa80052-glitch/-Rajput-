e3d93a7db97134dd8da4a01041f90b611cd6768f
gamerzh910-gif <gamerzh910@gmail.com>
2026-03-05 21:17:56 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_NAME = "prakharrrrrrrrr7"
+API_ID    = 221889293044
+API_HASH  = "099e3a1dce52b7jeej677299c3ab8ab3b6ca"
+BOT_TOKEN = "8774729055:AAFFK30VCT1bg_gHG-Q98h1K3PndR1BBuNQ"
+#MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
+MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
