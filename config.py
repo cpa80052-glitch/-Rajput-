a0c7f3b9a53b0547a0d28b48be54dd5155ca2cd2
cpa80052-glitch <cpa80052@gmail.com>
2026-03-06 11:10:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 BOT_NAME = "prakharrrrrrrrr7"
-API_ID    = 221889293044
-API_HASH  = "099e3a1dce52b7jeej677299c3ab8ab3b6ca"
+API_ID    = 28389286
+API_HASH  = "b88da5f4f338cca30f8ea5fb53cb083b"
 BOT_TOKEN = "8774729055:AAFFK30VCT1bg_gHG-Q98h1K3PndR1BBuNQ"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+
